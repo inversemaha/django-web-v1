9a205911bbc51d7728327a3b858de40cc93199db
inversemaha <mahajur88@gmail.com>
2025-07-30 02:27:21 +0600

    :pencil2: Add login page for user

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
@@ -27,7 +28,29 @@ def register(request):
                     messages.error(request, f"{field}: {error}")
     else:
         form = UserCreationForm()
-    return render(request, 'main/register.html', context={'form': form})
+        return render(request, 'main/register.html', context={'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('main:homepage')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+        return render(request, 'main/login.html', context={'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'main/login.html', context={'form': form})
 
 def logout_view(request):
     logout(request)

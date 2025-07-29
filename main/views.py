7db9645165fdf45ea919a6cfcb7b5657dd6e6f68
inversemaha <mahajur88@gmail.com>
2025-07-29 12:54:44 +0600

    :pencil2: Add static css and home page html with data

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Tutorial
 # Create your views here.
 def homepage(request):
-    return HttpResponse("Welcome to the homepage!")
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'tutorials': Tutorial.objects.all()})

ed5b30e535017b1698c8c45cc090cc9d0e1c9549
inversemaha <mahajur88@gmail.com>
2025-07-30 00:50:33 +0600

    :pencil2: Add template design and static css with register page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def homepage(request):
     return render(request=request,
                   template_name='main/home.html',
                   context={'tutorials': Tutorial.objects.all()})
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,
+                  'main/register.html',
+                  context={'form': form})
